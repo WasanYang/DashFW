85bf35f5a13599a509730495e195a39b0c640541
Wasan <evol1996@outlook.com>
2026-04-06 00:23:04 +0700

    ✨ refactor(page): improve authentication handling and add pathname dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import app from '@/lib/firebase';
+"use client";
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "@/lib/firebase";
 
 export default function Page() {
   const router = useRouter();
+  const pathname = usePathname();
+
   useEffect(() => {
     const auth = getAuth(app);
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (!user) {
-        router.replace('/login');
+        router.replace("/login");
+      } else if (pathname === "/") {
+
+        router.replace("/dashboard");
       }
     });
     return () => unsubscribe();
-  }, [router]);
+  }, [router, pathname]);
   return null;
 }
